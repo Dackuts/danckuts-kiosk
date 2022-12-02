fbf950b75cac5961298063c4bef1342a02d613a4
timothycarambat <rambat1010@gmail.com>
2022-12-01 16:34:51 -0800

    Modify logic for displaying the error message for if the user is late or early for an appointment restrict scope to todays appointments, then look ahead and show a relevant error message depending on the case resolves #8

--- src/routes/CheckIn.js
+++ src/routes/CheckIn.js
@@ -15,16 +15,27 @@ export default function CheckIn({ profile, location }) {
   const [openScheduler, setOpenScheduler] = useState(false);
   const [seconds, setSeconds] = useState(15);
   const [appointments, setAppointments] = useState([]);
+  const [missedTodaysAppt, setMissedTodaysAppt] = useState(null)
 
   useEffect(() => {
     async function fetchData() {
-      const { future } = await getAppointments();
+      const { future, past } = await getAppointments();
 
       const appointments = future.filter((a) =>
         profile.id === "user" ? a.dependent == null : a.dependent === profile.id
       );
+      const pastAppts = past.filter((a) =>
+        profile.id === "user" ? a.dependent == null : a.dependent === profile.id
+      );
+
+      const missedTodays = pastAppts.find((a) =>
+        DateTime.now().hasSame(DateTime.fromISO(a.date), "day") &&
+        DateTime.now().toMillis() > DateTime.fromISO(a.date).toMillis() &&
+        !a?.checkedin
+      )
 
       setAppointments(appointments);
+      setMissedTodaysAppt(missedTodays)
       setLoading(false);
       startTimer();
     }
@@ -49,6 +60,30 @@ export default function CheckIn({ profile, location }) {
     sessionStorage.clear("token");
     // eslint-disable-next-line no-restricted-globals
     window.location.reload();
+  }
+
+  if (!!missedTodaysAppt) {
+    return (
+      <div>
+        <div className={styles["image-container"]}>
+          <img className={styles.image} src={Caution} alt="caution" />
+        </div>
+        <p className={styles.name}>Hey {profile.first_name},</p>
+        <p className={styles.message}>
+          It's too late to check in to your{" "}
+          {DateTime.fromISO(missedTodaysAppt.date).toFormat("h:mm a DD")}{" "}
+          appointment. Please speak to the manager for assistance.
+        </p>
+        <div className={styles["button-container"]}>
+          <div
+            className={styles.button}
+            onClick={finish}
+          >
+            DONE
+          </div>
+        </div>
+      </div>
+    )
   }
 
   return loading ? (
@@ -154,10 +189,7 @@ export default function CheckIn({ profile, location }) {
             <div className={styles["button-container"]}>
               <div
                 className={styles.button}
-                onClick={async () => {
-                  await postCheckIn(appointments[0].id);
-                  finish();
-                }}
+                onClick={finish}
               >
                 DONE
               </div>
@@ -174,13 +206,7 @@ export default function CheckIn({ profile, location }) {
             </div>
             <p className={styles.name}>Hey {profile.first_name},</p>
             <p className={styles.message}>
-              You are too early to check in for your{" "}
-              {DateTime.fromISO(appointments[0].date).toFormat("h:mm a DD")}{" "}
-              appointment. You cannot check in sooner than{" "}
-              {DateTime.fromISO(appointments[0].date)
-                .minus({ minutes: 20 })
-                .toFormat("h:mm a")}{" "}
-              . Enjoy a drink, hang out and check back in soon!
+              {earlyMessage(appointments[0])}
             </p>
             <div className={styles["button-container"]}>
               <div className={styles.button} onClick={finish}>
@@ -196,4 +222,37 @@ export default function CheckIn({ profile, location }) {
       </p>
     </>
   );
+}
+
+function earlyMessage(appointment) {
+  if (DateTime.now().hasSame(DateTime.fromISO(appointment.date), "day")) {
+    return (
+      <>
+        You are too early to check in for your{" "}
+        {DateTime.fromISO(appointment.date).toFormat("h:mm a DD")}{" "}
+        appointment.
+        <br />
+        <br />
+        You cannot check in sooner than{" "}
+        {DateTime.fromISO(appointment.date)
+          .minus({ minutes: 20 })
+          .toFormat("h:mm a")}{" "}
+        .
+        <br />
+        <br />
+        Enjoy a drink, hang out and check back in soon!
+      </>
+    )
+  } else {
+    return (
+      <>
+        You dont have an appointment for today that you can check in for.
+        Your next appointment is scheduled for <b>{DateTime.fromISO(appointment.date).toFormat("DD @ hh:mm a")}</b>.
+        <br /><br />
+        Come back then to check in for your kut.
+        <br />
+        If you think this is an error you should speak to the manager on site.
+      </>
+    )
+  }
 }
